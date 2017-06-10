053bd90b99f0374471d4776b899582af3a4ed4c6
Lukedell <ldellisola@itba.edu.ar>
2017-06-09 21:43:41 -0300

    Cosas de netbeans

--- build/Debug/GNU-Linux/Allegro_operations.o.d
+++ build/Debug/GNU-Linux/Allegro_operations.o.d
@@ -1,6 +1,8 @@
 build/Debug/GNU-Linux/Allegro_operations.o: Allegro_operations.c \
- Allegro_operations.h Common_definitions.h
+ Allegro_operations.h Common_definitions.h Allegro_IO.h
 
 Allegro_operations.h:
 
 Common_definitions.h:
+
+Allegro_IO.h:
